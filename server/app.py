3aa1e8dad2932944d1f83cbba6b227befcfd36c6
Rafiul Sabbir <mdrhri-6@B0497.macstaff.ltu.se>
2016-11-19 18:35:25 +0100

    Added offers in json

--- server/app.py
+++ server/app.py
@@ -1,15 +1,16 @@
 # @Author: mdrhri-6
 # @Date:   2016-11-19T10:31:52+01:00
 # @Last modified by:   mdrhri-6
-# @Last modified time: 2016-11-19T15:22:30+01:00
+# @Last modified time: 2016-11-19T16:08:46+01:00
 
-
+# from __future__ import unicode_literals
+# import unicodedata
 from flask import Flask, request, json
-from flask_json import FlaskJSON, JsonError, \
-    json_response, as_json
+from flask_json import FlaskJSON, JsonError, json_response, as_json
 import sys
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 
 FlaskJSON(app)
 
