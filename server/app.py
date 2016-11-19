be133dc5896ea0dadc1c42e22a15c23bea3f851e
Rafiul Sabbir <mdrhri-6@B0497.macstaff.ltu.se>
2016-11-19 13:39:12 +0100

    Updated app.py

--- server/app.py
+++ server/app.py
@@ -1,17 +1,23 @@
 # @Author: mdrhri-6
 # @Date:   2016-11-19T10:31:52+01:00
 # @Last modified by:   mdrhri-6
-# @Last modified time: 2016-11-19T11:03:24+01:00
+# @Last modified time: 2016-11-19T13:37:45+01:00
 
 
-
+from __future__ import print_function
 from flask import Flask
+from flask import json
+import sys
 
 app = Flask(__name__)
 
 @app.route("/api/getDetails/<int:id>")
 def hello(id):
-    return "Hello Worlds!!" + str(id)';'
+    # with open('data.json') as data_file:
+    #     data = json.loads(data_file)
+    # print (data, file = sys.stderr)
+    data = json.loads(open("data.json"))
+    return data
 
 
 if __name__ == '__main__':
